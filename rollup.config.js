f4f8bc39db1bf730b417dc80dd9fb0dca6e6dca0
klm <58208740+klm-lab@users.noreply.github.com>
2023-10-12 18:55:50 +0000

    Add esm build (#8)
    * Add esm build
    
    * Update readme

---
+++ rollup.config.js
@@ -0,0 +1,72 @@
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const terser = require("@rollup/plugin-terser");
+const copy = require("rollup-plugin-copy");
+
+module.exports = [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: "lib/index.js",
+        format: "cjs"
+      },
+      {
+        file: "lib/index.min.js",
+        format: "cjs",
+        plugins: [terser()]
+      },
+      {
+        file: "lib/index.esm.js",
+        format: "esm"
+      },
+      {
+        file: "lib/index.esm.min.js",
+        format: "esm",
+        plugins: [terser()]
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" })
+    ],
+    external: [/node_modules/]
+  },
+  {
+    input: "./src/react/index.ts",
+    output: [
+      {
+        file: "lib/react/index.js",
+        format: "cjs"
+      },
+      {
+        file: "lib/react/index.min.js",
+        format: "cjs",
+        plugins: [terser()]
+      },
+      {
+        file: "lib/react/index.esm.js",
+        format: "esm"
+      },
+      {
+        file: "lib/react/index.esm.min.js",
+        format: "esm",
+        plugins: [terser()]
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      copy({
+        targets: [
+          { src: "./package.json", dest: "lib/" },
+          { src: "./README.md", dest: "lib/" }
+        ]
+      })
+    ],
+    external: ["react", "react-dom", /node_modules/]
+  }
+];
--- webpack.config.dev.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-// @ts-ignore
-import { config } from "./webpack.config";
-
-module.exports = {
-  ...config,
-  mode: "development",
-  devtool: false
-};
